f2a4c24ad13dd5fbda7ddacf7651e867733dde05
TheBeeMC <thebeemcnetwork@gmail.com>
2018-08-28 01:32:18 +0700

    Update run.py

--- run.py
+++ run.py
@@ -6,12 +6,18 @@ import time
 import os
 
 
-
 Client = discord.Client()
 
 
+         
+                                      
 @client.event
 async def on_ready():
       await client.change_presence(game=discord.Game(name="Message me for help!"))
-    
+        
+        
+        
+                    
+
+
 client.run(os.getenv('TOKEN'))
