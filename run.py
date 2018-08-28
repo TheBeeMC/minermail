7e60de615978333b0a4a1ed153326e2621395958
TheBeeMC <thebeemcnetwork@gmail.com>
2018-08-28 11:27:17 +0700

    Update run.py

--- run.py
+++ run.py
@@ -2,35 +2,22 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
-import os
 import time
+import os
 
 
-Client = discord.Client()
-client = commands.Bot(command_prefix = "!")
+Client = discord.Client() #Initialise Client 
+client = commands.Bot(command_prefix = "?") #Initialise client bot
 
 
-@client.event
+@client.event 
 async def on_ready():
-    print("Bot is online and connected to Discord")
-
+    print("Bot is online and connected to Discord") #This will be called when the bot connects to the server
 
 @client.event
 async def on_message(message):
-    if message.content.upper().startswith('$PING'):
-        userID = message.author.id
-        await client.send_message(message.channel, "<@%s> Pong!" % (userID))
-    if message.content.upper().startswith('!SAY'):
-        if message.author.id == "<user id>": #Replace <User ID> with the ID of the user you want to be able to execute this command!
-            args = message.content.split(" ")
-            await client.send_message(message.channel, "%s" % (" ".join(args[1:])))
-        else:
-            await client.send_message(message.channel, "You do not have permission")
-    if message.content.upper().startswith('!AMIADMIN'):
-        if "<role id>" in [role.id for role in message.author.roles]: #Replace <Role ID> with the ID of the role you want to be able to execute this command
-            await client.send_message(message.channel, "You are an admin")
-        else:
-            await client.send_message(message.channel, "You are not an admin")
+    if message.content == "cookie":
+        await client.send_message(message.channel, ":cookie:") #responds with Cookie emoji when someone says "cookie"
         
 
  @client.event
