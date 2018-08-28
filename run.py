3a798275fb1ee4873293ddfd2d39bc2f1c129cbe
TheBeeMC <thebeemcnetwork@gmail.com>
2018-08-28 11:48:16 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,46 +1,41 @@
 import discord
+from discord.ext import commands
 import random
+import asyncio
+import time
+import os
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged in as')
-        print(self.user.name)
-        print(self.user.id)
-        print('------')
 
-    async def on_message(self, message):
-        # we do not want the bot to reply to itself
-        if message.author.id == self.user.id:
-            return
+Client = discord.Client()
 
-        if message.content.startswith('$guess'):
-            await message.channel.send('Guess a number between 1 and 10.')
+client = commands.Bot(command_prefix = "?")
 
-            def is_correct(m):
-                return m.author == message.author and m.content.isdigit()
+chat_filter = ["FUCK", "CUNT", "BITCH", "DICK", "KYS", "FAGGOT", "FUCKING", "NIGGER", "NIGGA", "WHORE", "ASS", "KILL YOUR SELF", "IDIOT", "DUMBASS", "LOSER"]
+bypass_list = []
 
-            answer = random.randint(1, 10)
-
-            try:
-                guess = await self.wait_for('message', check=is_correct, timeout=5.0)
-            except asyncio.TimeoutError:
-                return await message.channel.send('Sorry, you took too long it was {}.'.format(answer))
-
-            if int(guess.content) == answer:
-                await message.channel.send('You are right!')
-            else:
-                await message.channel.send('Oops. It is actually {}.'.format(answer))
-
-                     
+@client.event
+async def on_message(message):
+    contents = message.content.split(" ") #contents is a list type
+    for word in contents:
+        if word.upper() in chat_filter:
+            if not message.author.id in bypass_list:
+                try:
+                    await client.delete_message(message)
+                    await client.send_message(message.channel, "**Hey!** Don't swear 😡 Thank you")
+                except discord.errors.NotFound:
+                    return
+         
+                
+                                 
 @client.event
 async def on_ready():
-      await client.change_presence(activity=discord.Game(name="Create a ticket for help!!"))                      
-                     
-                     
-                     
-                     
-client = MyClient()
-       
-client.run(os.getenv('TOKEN'))           
+      await client.change_presence(game=discord.Game(name="& Mining ⛏"))
+        
+        
+        
+                    
+
+
+client.run(os.getenv('TOKEN'))     
             
                        
