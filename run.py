b679c356645461c70c1ea7b0c1d22eaeeeb96218
TheBeeMC <thebeemcnetwork@gmail.com>
2018-08-28 11:57:09 +0700

    Update run.py

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ Client = discord.Client()
                                                  
 @client.event
 async def on_ready():
-      await client.change_presence(activity=discord.Game(name="& Mining ⛏"))
+      await client.change_presence(game=discord.Game(name="& Mining ⛏"))
         
         
         
